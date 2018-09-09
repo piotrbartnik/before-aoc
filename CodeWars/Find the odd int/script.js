0237ef8cc222fb4a5ef2251e6f878d9f63908432
piotrekbartnik <piotr.bartnik@hotmail.com>
2018-09-09 21:27:10 +0200

    added rigth js funcion

--- CodeWars/Find the odd int/script.js
+++ CodeWars/Find the odd int/script.js
@@ -1,10 +1,19 @@
 function findOdd() {
-let input = document.querySelector(".text__area").value;
+
+  let input = document.querySelector(".excercise__textarea").value;
+
+  input = input.split(/[,\s\-]/)
+
+  for (var i = input.length - 1; i--;) {
+    if (input[i] === ',' || input[i] === "") input.splice(i, 1);
+  }
+  
   let counts = {};
-  e.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+  input.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+  console.log(counts);
   for (i = 0; i < Object.keys(counts).length; i++) {
     if (Math.abs(Object.values(counts)[i] % 2) == 1) {
-      return parseInt(Object.keys(counts)[i]);
+      console.log(parseInt(Object.keys(counts)[i]));
     }
-  } return "There is no such object that amount in array is odd"
+  } console.log("There is no such object that amount in array is odd");
 }
